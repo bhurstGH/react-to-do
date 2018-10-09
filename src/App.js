070becfceb27a07bf0716cc69c4080e3722be02b
Brett Hurst <bretthurst@outlook.com>
2018-10-08 23:28:58 -0700

    Added deleteToDo function.

--- src/App.js
+++ src/App.js
@@ -33,12 +33,25 @@ class App extends Component {
     this.setState({todos: todos});
   }
 
+  deleteToDo(index) {
+    this.setState({
+      todos: this.state.todos.filter((todo, i) => {
+        return i !== index;
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={index}  description={todo.description} isCompleted={todo.isCompleted} toggleComplete={() => this.toggleComplete(index)}/>
+            <ToDo key={index}
+              description={todo.description}
+              isCompleted={todo.isCompleted}
+              toggleComplete={() => this.toggleComplete(index)}
+              deleteToDo={() => this.deleteToDo(index)}
+            />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
